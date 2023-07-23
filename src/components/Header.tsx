8dc255b5daaa039977ad2a116bba7dc4e72af01a
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2023-07-23 20:32:28 +0900

    adjust header and join fomr (useState)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,31 +8,27 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 103px;
-
   background-color: whitesmoke;
 `;
 
-/* 질문: onClick 이벤트 추가하려면 무조건 button 이어야 되나? */
 const Logo = styled.button`
-  padding-left: 128px;
   cursor: pointer;
   background: none;
   border: none;
 
-  color: #2C323A;
+  color: #2c323a;
   font-family: Pretendard;
   font-size: 30px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
   letter-spacing: 2.2px;
-`
+`;
 
 const Menu = styled.button`
-  padding-right: 80px;
   display: inline-block;
   cursor: pointer;
-  color: #2C323A;
+  color: #2c323a;
   text-align: center;
   font-family: Pretendard;
   font-size: 20px;
@@ -42,45 +38,52 @@ const Menu = styled.button`
   text-transform: uppercase;
   background: none;
   border: none;
-`
+  margin-left: 82px;
+`;
 
 const NavItem = styled.ul`
-  padding-left: 84px;
-  margin-right: 843px;
-`
+  display: flex;
+`;
 
-/* 질문1) 로그인 버튼이 헤더에 고정적으로 위치하면 안될 것 같은데, 어떻게 넣지? 
-   질문2) 배열 for문에 돌릴 때 i 만 갈아끼우는 것처럼, hanedleClick const 를 저렇게 개별적으로 생성하는 것이 아니라 효율적으로 생성하는 방법 없을까?
-*/
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1920px;
+  padding-left: 128px;
+  padding-right: 128px;
+  align-items: center;
+  margin: 0 auto;
+  display: flex;
+`;
 
 function Header() {
-  
   const navigate = useNavigate();
   const handleLoginClick = () => {
     navigate("/login");
-  }
+  };
   const handleLogoClick = () => {
     navigate("/");
-  }
+  };
   const handleMenu1Click = () => {
     navigate("/previous");
-  }
+  };
   const handleMenu2Click = () => {
     navigate("/myboard");
-  }
+  };
   const handleMenu3Click = () => {
     navigate("/community");
-  }
+  };
 
   return (
     <Container>
-      <Logo onClick={handleLogoClick}>Kupply</Logo>
-      <NavItem>
-        <Menu onClick={handleMenu1Click}>PREVIOUSLY</Menu>
-        <Menu onClick={handleMenu2Click}>MYBOARD</Menu>
-        <Menu onClick={handleMenu3Click}>COMMUNITY</Menu>
-      </NavItem>
-      <LoginHButton onClick={handleLoginClick}/>
+      <Wrapper>
+        <Logo onClick={handleLogoClick}>Kupply</Logo>
+        <NavItem>
+          <Menu onClick={handleMenu1Click}>PREVIOUSLY</Menu>
+          <Menu onClick={handleMenu2Click}>MYBOARD</Menu>
+          <Menu onClick={handleMenu3Click}>COMMUNITY</Menu>
+        </NavItem>
+        <LoginHButton onClick={handleLoginClick} />
+      </Wrapper>
     </Container>
   );
 }
