236ebe8861392a9b2d22f277372551b67331c923
overthestream <njhnjh02@naver.com>
2023-08-03 21:45:23 +0900

    use global style

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,28 @@ import CommunityPage from "./pages/CommunityPage";
 import JoinPage from "./pages/JoinPage";
 import MessagePage from "./pages/MessagePage";
 import SettingsPage from "./pages/SettingsPage";
-import LabelPrimaryButton from "./assets/buttons/label/LabelPriButton";
-import LabelPrimaryWideButton from "./assets/buttons/label/LabelPriWideButton";
-import IconSecButton from "./assets/buttons/icon/IconSecButton";
-import IconPriButton from "./assets/buttons/icon/IconPriButton";
+import { GlobalStyle } from "./globalStyle";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header menu="join" />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/previous" element={<PreviousPage />} />
-          <Route path="/myboard" element={<MyBoardPage />} />
-          <Route path="/community" element={<CommunityPage />} />
-          <Route path="/join" element={<JoinPage />} />
-          <Route path="/message" element={<MessagePage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-        </Routes>
-        <IconSecButton />
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<GlobalStyle />
+			<div className="App">
+				<Header menu="join" />
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/previous" element={<PreviousPage />} />
+					<Route path="/myboard" element={<MyBoardPage />} />
+					<Route path="/community" element={<CommunityPage />} />
+					<Route path="/join" element={<JoinPage />} />
+					<Route path="/message" element={<MessagePage />} />
+					<Route path="/settings" element={<SettingsPage />} />
+				</Routes>
+				<Footer />
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
