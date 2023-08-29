ce238031f8fc631cba2a1f20ee8da9d24c1fa76c
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-08-29 16:53:47 +0900

    asset: verificationBox

--- src/assets/VerificationBox.tsx
+++ src/assets/VerificationBox.tsx
@@ -1,57 +1,59 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import styled from "styled-components";
 
-export interface VerificationBoxProps
-  extends React.ComponentPropsWithoutRef<"div"> {
-  active?: boolean;
+// 자동 넘어가기
+
+export interface VerificationBoxProps extends React.ComponentPropsWithoutRef<"input"> {
+  isEntered?: boolean;
+  value?: string;
+  onValueChange?: (value: string) => void;
 }
 
-const Container = styled.div`
+const InputWrapper = styled.input<VerificationBoxProps>`
   width: 75px;
   height: 75px;
-`;
-
-const BoxWrapper = styled.div<VerificationBoxProps>`
-  width: 74.762px;
-  height: 75px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border-radius: 5px;
-  border: ${(props) =>
-    props.active ? "1px solid #d85888" : "1px solid #B9B9B9"};
+  border: ${(props) => (props.isEntered ? "1px solid #D85888" : "1px solid #b9b9b9")};
+  box-shadow: ${(props) => (props.isEntered ? "0px 4px 12px 0px rgba(216, 88, 136, 0.2)" : "null")};
   background: var(--white, #fff);
-  box-shadow: ${(props) =>
-    props.active ? "0px 4px 12px 0px rgba(216, 88, 136, 0.2)" : "null"};
+  color: var(--main-black, #141414);
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 100%;
+  text-align: center;
+
+  &:focus {
+    border-color: #d85888;
+    outline: none;
+    box-shadow: 0px 4px 12px 0px rgba(216, 88, 136, 0.2);
+  }
 `;
 
 export default function VerificationBox(props: VerificationBoxProps) {
-  const { children, active = false, ...rest } = props; // 비활성화 상태로 초기화
-  return (
-    <Container {...rest}>
-      <BoxWrapper active={active}></BoxWrapper>
-    </Container>
-  );
-}
+  const { value, onValueChange } = props;
 
-/*
-function VerificationBox(props: VerificationBoxProps) {
-  const { children, active = "false", ...rest } = props;
-  return (
-    <Container {...rest}>
-      {active === true ? (
-        <img
-          src="../../design_image/verification_box/active.png"
-          width="75px"
-          height="75px"
-        />
-      ) : (
-        <img
-          src="../../design_image/verification_box/unactive.png"
-          width="75px"
-          height="75px"
-        />
-      )}
-    </Container>
-  );
-}
+  const [isEntered, setIsEntered] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>(value || "");
 
-export default VerificationBox;
-*/
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+
+    if (newValue === "") {
+      setIsEntered(false);
+    } else {
+      setIsEntered(true);
+    }
+
+    if (onValueChange) {
+      onValueChange(newValue);
+    }
+  };
+
+  return <InputWrapper onChange={handleChange} maxLength={1} isEntered={isEntered} value={inputValue} />;
+}
