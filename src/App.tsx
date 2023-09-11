e242a41d8f0e15fffaf778c81b32ba35bae42d15
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-09-11 22:37:56 +0900

    refactor: previous page & previous detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import styled from "styled-components";
-import Header from "./assets/Header";
-import Footer from "./components/Footer";
-import MainPage from "./pages/MainPage";
-import LoginPage from "./pages/LoginPage";
-import PreviousPage from "./pages/PreviousPage";
-import MyBoardPage from "./pages/MyBoardPage";
-import CommunityPage from "./pages/CommunityPage";
-import MessagePage from "./pages/MessagePage";
-import SettingsPage from "./pages/SettingsPage";
-import SignUp1Page from "./pages/SignUp/SignUp1Page";
-import SignUp2Page from "./pages/SignUp/SignUp2Page";
-import SignUp3Page from "./pages/SignUp/SignUp3Page";
-import { SignUp4Page, SignUp4PageCandidate, SignUp4PagePasser } from "./pages/SignUp/SignUp4Page";
-import { SignUp5Page, SignUp5Complete } from "./pages/SignUp/SignUp5Page";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from './assets/Header';
+import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import PreviousPage from './pages/PreviousPage';
+import PreviousDetailPage from './pages/PreviousDetailPage';
+import MyBoardPage from './pages/MyBoardPage';
+import CommunityPage from './pages/CommunityPage';
+import MessagePage from './pages/MessagePage';
+import SettingsPage from './pages/SettingsPage';
+import SignUp1Page from './pages/SignUp/SignUp1Page';
+import SignUp2Page from './pages/SignUp/SignUp2Page';
+import SignUp3Page from './pages/SignUp/SignUp3Page';
+import { SignUp4Page, SignUp4PageCandidate, SignUp4PagePasser } from './pages/SignUp/SignUp4Page';
+import { SignUp5Page, SignUp5Complete } from './pages/SignUp/SignUp5Page';
 
 export default function App() {
   return (
-    <div style={{ width: "1920px", height: "1510px", marginTop: "96px" }}>
+    <div style={{ width: '1920px', height: '1510px', marginTop: '96px' }}>
       <Header logined={false} />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/previous" element={<PreviousPage />} />
+        <Route path="/previous/:majorId" element={<PreviousDetailPage />} />
         <Route path="/myboard" element={<MyBoardPage />} />
         <Route path="/community" element={<CommunityPage />} />
         <Route path="/join" element={<SignUp1Page />} />
