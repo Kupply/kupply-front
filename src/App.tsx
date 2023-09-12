11fc5cfcad5051be25842e73623b2f18749fc130
kbs1115 <bruce11158@gmail.com>
2023-09-12 13:15:04 +0900

    hotfix: import Header 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import Header from './assets/Header';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
