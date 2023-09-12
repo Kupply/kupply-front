1534d1eb76be85398bcc9c049b6d729b71580cf8
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2023-09-13 05:24:01 +0900

    fix: 경로 수정

--- src/pages/SignUp/modals/SignUpSmall.tsx
+++ src/pages/SignUp/modals/SignUpSmall.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import Typography from '../../../assets/Typography';
 import AlertIconCheck from '../../../assets/icons/AlertIconCheck';
-import ModalSmall from '../../../components/ModalSmall';
+import ModalSmall from '../../../components/base/ModalSmall';
 
 // ref: https://velog.io/@chlgdnd/%EB%AA%A8%EB%8B%AC-%EC%B0%BD-Fade-out-%EC%95%A0%EB%8B%88%EB%A9%94%EC%9D%B4%EC%85%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0Feat.-React-Typescript
 
