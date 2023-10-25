a561937c3c6672a9614124ccfc47a79bca63a137
박유연 <princess_yuyeon@bag-yuyeon-ui-iMac.local>
2023-10-25 13:44:39 +0900

    landing

--- src/assets/landing/FAQbox.tsx
+++ src/assets/landing/FAQbox.tsx
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import Typography from '../Typography';
 
+export interface FAQboxProps extends React.ComponentPropsWithRef<'div'> {
+  question?: string;
+  answer?: string;
+}
+
 const MainWrapper = styled.div`
   display: inline-flex;
   width: 1578px;
@@ -28,21 +33,31 @@ const ContentWrapper = styled.div`
   gap: 6px;
 `;
 
-function FAQbox() {
+function FAQbox(props: FAQboxProps) {
+  const {
+    question = '이중전공 지원에 반영되는 학점은 대내용 학점인가요, 대외용 학점인가요?',
+    answer = '이중전공 지원에는 직전학기까지 수강신청한 모든 과목의 총 평점평균인 대내용 학점(F를 포함한 전체성적)이 반영됩니다.',
+    ...rest
+  } = props;
+
   const [showAnswer, setShowAnswer] = useState(false);
   const handleButtonClick = () => (showAnswer == true ? setShowAnswer(false) : setShowAnswer(true));
 
   return (
-    <MainWrapper style={{ border: showAnswer == true ? '1px solid #d85888' : '1px solid #b9b9b9' }}>
+    <MainWrapper style={{ border: showAnswer == true ? '1px solid #d85888' : '1px solid #b9b9b9' }} {...rest}>
       <TitleWrapper>
         <Typography size="largeText" bold={showAnswer == true ? '700' : '500'}>
-          이중전공 지원에 반영되는 학점은 대내용 학점인가요, 대외용 학점인가요?
+          {question}
         </Typography>
-        <img src="../../design_image/u_angle-down.svg" onClick={handleButtonClick} />
+        <img
+          src="../../design_image/u_angle-down.svg"
+          onClick={handleButtonClick}
+          style={{ cursor: 'pointer', transform: showAnswer == true ? 'rotate(180deg)' : 'none' }}
+        />
       </TitleWrapper>
       {showAnswer == true ? (
         <ContentWrapper>
-          <Typography size="mediumText">이중전공은 해당 학기에 아래 요건을 모두 만족하여야 지원 가능합니다.</Typography>
+          <Typography size="mediumText">{answer}</Typography>
         </ContentWrapper>
       ) : (
         <></>
--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -20,7 +20,7 @@ const Wrapper = styled.div`
   background: #fff;
   align-items: center;
   justify-content: center;
-  z-index: 3;
+  z-index: 1000;
 `;
 
 const FlexContainer = styled.div`
