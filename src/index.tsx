4d8d09405dfe15a8ff665c6c5da9979e7542e89a
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2023-10-30 14:42:44 +0900

    feat: ga4

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,12 @@ import { CookiesProvider } from 'react-cookie';
 import { GlobalStyle } from './globalStyle';
 import App from './App';
 import ScrollToTop from './components/ScrollToTop';
+import ReactGA from 'react-ga4';
+
+// 구글 애널리틱스 운영 서버에만 적용
+if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
