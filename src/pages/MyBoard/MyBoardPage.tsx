1ca3e51e86147d8c7385644643b141734056f0fa
coding-princess <pyykity01@naver.com>
2023-11-02 20:22:55 +0900

    fix

--- src/pages/MyBoard/MyBoardPage.tsx
+++ src/pages/MyBoard/MyBoardPage.tsx
@@ -471,7 +471,7 @@ export default function MyBoardPage() {
   const isWithinTimeRange = () => {
     const now = new Date();
     const startTime = new Date('2023-11-02T10:00:00'); //2023-11-08로 고쳐야함
-    const endTime = new Date('2023-11-10T:00:00');
+    const endTime = new Date('2023-11-10T24:00:00');
 
     return now >= startTime && now <= endTime;
   };
