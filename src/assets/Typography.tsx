a7e2c8e19d38c3172a0e7e8f74abc9843402b444
박유연 <princess_yuyeon@bag-yuyeon-ui-iMac.local>
2024-01-31 01:46:32 +0900

    maxFontSize

--- src/assets/Typography.tsx
+++ src/assets/Typography.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export interface TypographyProps extends React.ComponentPropsWithoutRef<'span'> {
   size?: string;
+  maxSize?: string;
   bold?: string;
   color?: string;
 }
