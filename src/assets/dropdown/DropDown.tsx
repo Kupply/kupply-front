967eb4c332c38e5330af84d549f4ca4efedb70cf
KUcodemaster <jh9485@korea.ac.kr>
2024-02-09 16:53:56 +0900

    드롭다운 에셋 정리

--- src/assets/dropdown/DropDown.tsx
+++ src/assets/dropdown/DropDown.tsx
@@ -59,7 +59,7 @@ function DropDown({ title, optionList, value, setValue }: DropDownProps) {
           <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M19.8333 10.6984C19.6147 10.4811 19.319 10.3591 19.0108 10.3591C18.7026 10.3591 18.4069 10.4811 18.1883 10.6984L14 14.8284L9.86998 10.6984C9.65139 10.4811 9.3557 10.3591 9.04748 10.3591C8.73926 10.3591 8.44357 10.4811 8.22498 10.6984C8.11563 10.8068 8.02884 10.9359 7.96961 11.078C7.91038 11.2202 7.87988 11.3727 7.87988 11.5267C7.87988 11.6807 7.91038 11.8332 7.96961 11.9754C8.02884 12.1176 8.11563 12.2466 8.22498 12.3551L13.1716 17.3017C13.2801 17.4111 13.4091 17.4979 13.5513 17.5571C13.6935 17.6163 13.846 17.6468 14 17.6468C14.154 17.6468 14.3065 17.6163 14.4487 17.5571C14.5908 17.4979 14.7199 17.4111 14.8283 17.3017L19.8333 12.3551C19.9427 12.2466 20.0295 12.1176 20.0887 11.9754C20.1479 11.8332 20.1784 11.6807 20.1784 11.5267C20.1784 11.3727 20.1479 11.2202 20.0887 11.078C20.0295 10.9359 19.9427 10.8068 19.8333 10.6984Z"
-              fill="#B9B9B9"
+              fill={isOpen ? '#D85888' : '#B9B9B9'}
             />
           </svg>
         </AngleDown>
@@ -150,12 +150,19 @@ const InputWrapper = styled.input<{ isOpen: boolean; isSelected: boolean }>`
   opacity: 0.8;
 
   ${(props) =>
-    ((props.isOpen && props.isSelected) || (!props.isOpen && props.isSelected)) &&
+    props.isOpen &&
     css`
       border: 1px solid #d85888;
       color: #d85888;
       opacity: 1;
     `}
+
+  /* ${(props) =>
+    props.isSelected &&
+    css`
+      color: #d85888;
+    `} */
+
 
   position: relative;
 
@@ -222,6 +229,7 @@ const OptionContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 629px;
+  height: 213px;
   /* height 의 경우 옵션의 개수에 따라 동적으로 변화하게 설정 */
   min-height: 0; /* 임의 설정 */
   overflow-y: auto; /* 최대 높이를 초과할 경우 스크롤바 추가 */
@@ -230,23 +238,35 @@ const OptionContainer = styled.div`
   align-items: center;
   padding-top: 17px;
   padding-bottom: 7px;
-
+  box-sizing: border-box;
   border-radius: 10px;
   border: 1px solid rgba(216, 88, 136, 0.2);
   background: #fff;
   box-shadow: 0px 0px 15px 0px rgba(79, 13, 37, 0.15);
+
+  /* 
+    스크롤바 제거
+    ( 크롬, 사파리, 오페라, 엣지 ) 동작 
+  */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none; /* 인터넷 익스플로러 */
+  scrollbar-width: none; /* 파이어폭스 */
 `;
 
 const OptionWrapper = styled.button<{
   isSelectedValue: boolean;
 }>`
   display: flex;
-  width: 570.04px;
-  height: 34px;
+  justify-content: space-between;
+  width: 95%;
+
+  /* height: 25%; */
   align-items: center;
   border-radius: 5px;
-  justify-content: space-between;
-  padding: 8px 18px;
+
+  padding: 13px 18px;
   margin-top: 4px;
 
   color: var(--main-black, #141414);
