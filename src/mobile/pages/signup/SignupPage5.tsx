d35bac154b63303c969a05defa55c461f23b803a
Hajin Yun <faiith7001@gmail.com>
2024-04-10 15:49:53 +0900

    Card02 수정

--- src/mobile/pages/signup/SignupPage5.tsx
+++ src/mobile/pages/signup/SignupPage5.tsx
@@ -1,2 +1,13 @@
-import React from "react";
+import { SignUpPageWrapper } from "../../components/signup/SignUpPageWrapper";
+import React, {useState, useEffect, useRef} from "react";
+import { useNavigate } from "react-router-dom";
+import styled, {css} from "styled-components";
+// import { CheckBoxButton01 } from "../../assets/buttons/CheckBoxButton";
+// import { ScrollLarge, ScrollSmall } from "../../assets/scroll/Scroll";
+// import { TermsText1, TermsText2 } from "../../components/signUp/TermsText";
+// import Button04 from "../../assets/buttons/Button04";
+// import Button03 from "../../assets/buttons/Button03";
+// import CTA01 from "../../assets/CTAs/CTA01";
+// import Typography from "../../assets/Typography";
+// import { join } from "../../utils/SignUpFunctions";
 // Scroll Asset이 만들어지기 전까지 하기 좀 그래서 일단 보류...
