e5f49bfd611698edc847ec35f917140d42b78b84
YoonJunHyeok <jhyoon1607@gmail.com>
2024-05-05 12:36:26 +0900

    fix: change on isLogined

--- src/components/onboarding/Preview1.tsx
+++ src/components/onboarding/Preview1.tsx
@@ -51,7 +51,7 @@ function Preview1() {
     };
 
     if (isLogined) loadData();
-  }, []);
+  }, [isLogined]);
 
   return (
     <MainWrapper>
@@ -90,7 +90,7 @@ function Preview1() {
             </CTA02>
           </ButtonWrapper>
         </>
-      ) : isDateInRange ? null : (
+      ) : !isDateInRange ? null : (
         <>
           <Blur />
           <ButtonWrapper>
