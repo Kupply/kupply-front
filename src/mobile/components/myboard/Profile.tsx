44968d3a77a2f7c2d781987136d279db62a961c7
coding-princess <pyykity01@naver.com>
2024-05-16 09:19:19 +0900

    hotfix

--- src/mobile/components/myboard/Profile.tsx
+++ src/mobile/components/myboard/Profile.tsx
@@ -36,7 +36,7 @@ const MobileProfile = ({
 
   const currentDate = new Date();
   const startDate = new Date('2024-05-10');
-  const endDate = new Date('2024-05-17');
+  const endDate = new Date('2024-05-31');
   const isDateInRange = currentDate >= startDate && currentDate <= endDate; // 해당 기간에만 True
 
   useEffect(() => {
