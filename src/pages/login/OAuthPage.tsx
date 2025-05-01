681a7f3949a0d38424266ba5dcfe4de2daf06211
YoonJunHyeok <jhyoon1607@gmail.com>
2025-05-01 13:49:10 +0900

    Fix: 고파스 자동 로그인 문구, redirecct

--- src/pages/login/OAuthPage.tsx
+++ src/pages/login/OAuthPage.tsx
@@ -29,18 +29,20 @@ export default function OAuthPage() {
           navigate('/');
           window.location.reload();
         } else {
-          setError('쿠플라이 회원이 아니에요. 고파스 아이디로 쿠플라이 서비스에 회원가입 해주세요.');
+          setError('쿠플라이 회원이 아니에요. \n고파스 아이디로 쿠플라이 서비스에 회원가입 해주세요.');
           setRedirectPath('/signup0');
         }
       } catch (err) {
         if (axios.isAxiosError(err) && err.response?.status === 401) {
           setError('고파스 인증에 실패했어요. 고파스 아이디로 직접 로그인 해주세요.');
+          setRedirectPath('/login');
         } else if (axios.isAxiosError(err) && err.response?.status === 403) {
           setError('고파스 강등 또는 미인증 회원은 쿠플라이 서비스를 이용할 수 없어요.');
+          setRedirectPath('/');
         } else {
           setError('알 수 없는 오류가 발생했어요. 잠시 후 다시 시도해 주세요.');
+          setRedirectPath('/');
         }
-        setRedirectPath('/');
       }
     };
 
