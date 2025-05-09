f4839aeb392cdfeab9cad246d1a9f777c48c154e
YoonJunHyeok <jhyoon1607@gmail.com>
2025-05-09 23:58:41 +0900

    Chore: format

--- src/mobile/pages/signup/NewSignUpPage1.tsx
+++ src/mobile/pages/signup/NewSignUpPage1.tsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from "react";
-import { useNewSignUp1Verification } from "../../../utils/SignUpFunctions";
-import { useNavigate } from "react-router-dom";
-import { api_url } from "../../../utils/HttpClient";
-import axios from "axios";
-import { SignUpPageWrapper } from "../../components/signup/SignUpPageWrapper";
-import styled from "styled-components";
-import Button03 from "../../assets/buttons/Button03";
-import Button04 from "../../assets/buttons/Button04";
-import Typography from "../../../assets/Typography";
-import { UserInput } from "../../components/signup/UserInput";
+import { useState, useEffect } from 'react';
+import { useNewSignUp1Verification } from '../../../utils/SignUpFunctions';
+import { useNavigate } from 'react-router-dom';
+import { api_url } from '../../../utils/HttpClient';
+import axios from 'axios';
+import { SignUpPageWrapper } from '../../components/signup/SignUpPageWrapper';
+import styled from 'styled-components';
+import Button03 from '../../assets/buttons/Button03';
+import Button04 from '../../assets/buttons/Button04';
+import Typography from '../../../assets/Typography';
+import { UserInput } from '../../components/signup/UserInput';
 
 export function SignUp1Page() {
   const [next, setNext] = useState(false);
-  const {complete, ID, pass} = useNewSignUp1Verification();
+  const { complete, ID, pass } = useNewSignUp1Verification();
   const navigate = useNavigate();
 
   // useEffect(() => {
-  //   // 최초 로그인 후 sign up1을 건너뛰고 싶다는 거자나 
-  //   // 그러니까 로그인을 하려고 입력을 했는데 아직 회원은 아닌 경우 
-  //   // 이거 완전히 잘못됐어. 
-  //   // uuid만 확인 하고 다른 설정을 안하고 있자나. 다른 것까지 다 하도록 만들어야 해 
+  //   // 최초 로그인 후 sign up1을 건너뛰고 싶다는 거자나
+  //   // 그러니까 로그인을 하려고 입력을 했는데 아직 회원은 아닌 경우
+  //   // 이거 완전히 잘못됐어.
+  //   // uuid만 확인 하고 다른 설정을 안하고 있자나. 다른 것까지 다 하도록 만들어야 해
   //   const uuid = localStorage.getItem('koreapasUUID');
   //   if (uuid) {
   //     sessionStorage.setItem('koreapasUUID', uuid);
@@ -30,49 +30,43 @@ export function SignUp1Page() {
   const handleSyncClick = () => {
     navigate('/sync0');
   };
-  
+
   const handleNext = async () => {
     if (ID === '' || pass === '') return;
-  
+
     const checkUrl = `${api_url}/auth/checkKoreapas`;
     const joinedUrl = `${api_url}/auth/checkKoreapasJoined`;
     const loginUrl = `${api_url}/auth/koreapasLogin`;
-  
+
     try {
       // Step 1: 인증 후 정보 받기
       const res = await axios.post(checkUrl, {
         id: ID,
         password: pass,
       });
-  
-      const {
-        koreapasUUID,
-        nickname,
-        studentId,
-        firstMajorCode,
-        firstMajorCampus,
-      } = res.data.data;
-  
+
+      const { koreapasUUID, nickname, studentId, firstMajorCode, firstMajorCampus } = res.data.data;
+
       sessionStorage.setItem('koreapasUUID', koreapasUUID);
       sessionStorage.setItem('nickname', nickname);
       sessionStorage.setItem('studentId', studentId);
       sessionStorage.setItem('firstMajorCode', firstMajorCode);
       sessionStorage.setItem('firstMajorCampus', firstMajorCampus);
-  
+
       // Step 2: UUID로 기존 가입 여부 확인
       const joinedRes = await axios.post(joinedUrl, { koreapasUUID });
       const alreadyJoined = joinedRes.data.data.alreadyJoined;
-      console.log('Already Joined??', alreadyJoined)
-  
+      console.log('Already Joined??', alreadyJoined);
+
       if (alreadyJoined) {
-        alert('이미 가입된 사용자입니다. 자동으로 로그인됩니다!')
+        alert('이미 가입된 사용자입니다. 자동으로 로그인됩니다!');
         // ✅ 이미 가입한 유저 → 로그인 API 호출
         const loginRes = await axios.post(loginUrl, {
           id: ID,
           password: pass,
           isRememberOn: false, // 또는 isChecked (checkbox 상태) 전달
         });
-  
+
         localStorage.setItem('accessToken', loginRes.data.data.accessToken);
         localStorage.setItem('refreshToken', loginRes.data.data.refreshToken);
         localStorage.setItem('isLogin', 'true');
@@ -82,7 +76,6 @@ export function SignUp1Page() {
         // ❌ 신규 유저 → sign-up flow로
         navigate('/signUp2');
       }
-  
     } catch (err: any) {
       if (axios.isAxiosError(err) && err.response?.status === 401) {
         alert('유효하지 않은 고파스 아이디 혹은 비밀번호에요.');
@@ -93,76 +86,71 @@ export function SignUp1Page() {
       }
     }
   };
-  
-  
-  
+
   const handlePrev = () => {
     navigate('/');
-  }
+  };
 
   const handleForgetClick = () => {
     window.open('https://www.koreapas.com/bbs/lostid_new.php', '_blank', 'noopener,noreferrer');
   };
 
-
   return (
     <SignUpPageWrapper step={1} stepInfo="고려대 학생 인증하기">
-    <div style={{marginTop: '14.5vw', marginBottom: '12.3vw', textAlign: 'center'}}>
-      <Typography
-        size="6.66vw"
-        bold="700"
-        style={{ lineHeight: '131.579%', textAlign: 'center' }}
-      >
-        환영합니다!
-      </Typography>
+      <div style={{ marginTop: '14.5vw', marginBottom: '12.3vw', textAlign: 'center' }}>
+        <Typography size="6.66vw" bold="700" style={{ lineHeight: '131.579%', textAlign: 'center' }}>
+          환영합니다!
+        </Typography>
 
-      <div style={{ marginTop: '4.6vw', textAlign: 'center' }}>
-        <span style={{ fontSize: '4vw', fontWeight: 500, opacity: 0.8 }}>
-          쿠플라이는
-        </span>
-        <img
-          src={process.env.PUBLIC_URL + `/designImage/login/tigerEmoji.png`}
-          alt="tigerEmoji"
-          style={{
-            height: '4.329vw',
-            verticalAlign: 'middle',
-            margin: '0 0.4vw',
-          }}
-        />
-        <span style={{ fontSize: '4vw', fontWeight: 700, opacity: 0.8 }}>
-          고파스 아이디/비밀번호
-        </span>
-        <br />
-        <span style={{ fontSize: '4vw', fontWeight: 500, opacity: 0.8 }}>
-          로 서비스 이용이 가능합니다.
-        </span>
+        <div style={{ marginTop: '4.6vw', textAlign: 'center' }}>
+          <span style={{ fontSize: '4vw', fontWeight: 500, opacity: 0.8 }}>쿠플라이는</span>
+          <img
+            src={process.env.PUBLIC_URL + `/designImage/login/tigerEmoji.png`}
+            alt="tigerEmoji"
+            style={{
+              height: '4.329vw',
+              verticalAlign: 'middle',
+              margin: '0 0.4vw',
+            }}
+          />
+          <span style={{ fontSize: '4vw', fontWeight: 700, opacity: 0.8 }}>고파스 아이디/비밀번호</span>
+          <br />
+          <span style={{ fontSize: '4vw', fontWeight: 500, opacity: 0.8 }}>로 서비스 이용이 가능합니다.</span>
+        </div>
       </div>
-    </div>
-    <ContentsList>
-      <ContentsWrapper>
-        <div>
-        <Typography size="3.33vw" bold="700">고파스 아이디</Typography>
-        <Typography size="3.33vw" bold="400">를 입력해주세요.</Typography>
-        </div>
-        <UserInput userInfoType="koreapasID" toNext={next}/>
-      </ContentsWrapper>
-      <ContentsWrapper>
-        <div>
-        <Typography size="3.33vw" bold="700">고파스 비밀번호</Typography>
-        <Typography size="3.33vw" bold="400">를 입력해주세요.</Typography>
-        </div>
-        <UserInput userInfoType="koreapasPass" toNext={next}/>
-      </ContentsWrapper>
-      <LinkBox>
-          <Link onClick={handleForgetClick}>고파스 아이디 비밀번호 찾기</Link> 
+      <ContentsList>
+        <ContentsWrapper>
+          <div>
+            <Typography size="3.33vw" bold="700">
+              고파스 아이디
+            </Typography>
+            <Typography size="3.33vw" bold="400">
+              를 입력해주세요.
+            </Typography>
+          </div>
+          <UserInput userInfoType="koreapasID" toNext={next} />
+        </ContentsWrapper>
+        <ContentsWrapper>
+          <div>
+            <Typography size="3.33vw" bold="700">
+              고파스 비밀번호
+            </Typography>
+            <Typography size="3.33vw" bold="400">
+              를 입력해주세요.
+            </Typography>
+          </div>
+          <UserInput userInfoType="koreapasPass" toNext={next} />
+        </ContentsWrapper>
+        <LinkBox>
+          <Link onClick={handleForgetClick}>고파스 아이디 비밀번호 찾기</Link>
           <Link onClick={handleSyncClick}>쿠플라이의 기존 회원이신가요?</Link>
-      </LinkBox>
-    </ContentsList>
-    <ButtonsWrapper>
-      <Button04 onClick={handlePrev} style={{width:'25.582%'}}/>
-      <Button03 state={complete? 'default' : 'disabled'} onClick={handleNext} style={{width: '74.418%'}}/>
-    </ButtonsWrapper>
-  </SignUpPageWrapper>
+        </LinkBox>
+      </ContentsList>
+      <ButtonsWrapper>
+        <Button04 onClick={handlePrev} style={{ width: '25.582%' }} />
+        <Button03 state={complete ? 'default' : 'disabled'} onClick={handleNext} style={{ width: '74.418%' }} />
+      </ButtonsWrapper>
+    </SignUpPageWrapper>
   );
 }
 
@@ -186,7 +174,6 @@ const ButtonsWrapper = styled.div`
   width: 100%;
 `;
 
-
 const LinkBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -194,7 +181,7 @@ const LinkBox = styled.div`
   justify-content: center;
   gap: 2.5vw;
   margin-top: 12.11vw;
-//   margin-bottom: 2.53vw;
+  //   margin-bottom: 2.53vw;
 `;
 
 const Link = styled.button`
@@ -207,4 +194,3 @@ const Link = styled.button`
   text-decoration-line: underline;
   text-transform: uppercase;
 `;
-
