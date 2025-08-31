8b03b867f8fde66b11b40def68997811b49f212b
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2025-08-31 22:59:31 +0900

    9월 과도기 임시조치

--- src/common/LastThreeSemesters.tsx
+++ src/common/LastThreeSemesters.tsx
@@ -3,19 +3,26 @@ function getLastThreeSemesters(): [string, string, string] {
   const currentYear = currentDate.getFullYear();
   const currentMonth = currentDate.getMonth(); // January is 0, December is 11
 
+
   // Determine the current semester based on the current month (assuming semester 1 is Jan-Jun, semester 2 is Jul-Dec)
   let currentSemester;
-  // if (currentMonth >= 2 && currentMonth <= 7) {
+
+
+  // Correct ver.
+  // if (currentMonth <= 5) {
   //   currentSemester = 1; // First semester (Spring)
   // } else {
   //   currentSemester = 2; // Second semester (Fall)
   // }
 
-  if (currentMonth <= 5) {
+
+  // Temporary ver. (2025.08.31 과도기 버전)
+  if (currentMonth <= 8) {
     currentSemester = 1; // First semester (Spring)
   } else {
     currentSemester = 2; // Second semester (Fall)
   }
+
 
   // Calculate the semesters
   if (currentSemester === 1) {
@@ -23,9 +30,11 @@ function getLastThreeSemesters(): [string, string, string] {
     return [`${currentYear - 1}-2`, `${currentYear - 1}-1`, `${currentYear - 2}-2`];
   } else {
     // If it's the second semester
-    // return [`${currentYear}-1R`, `${currentYear}-2R`, `${currentYear - 1}-1R`];
     return [`${currentYear}-1`, `${currentYear - 1}-2`, `${currentYear - 1}-1`];
   }
+
+
+
 }
 
 export const LastThreeSemesters = getLastThreeSemesters();
